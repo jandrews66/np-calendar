6bf8a037f5884a61f6e09979db67d518bde249e4
Joe Andrews <joeandrews92@gmail.com>
2024-09-15 13:41:14 -0700

    Create duplicateBookings function for Dashboard.jsx to check for duplicate bookings and display any to admin.

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -20,10 +20,39 @@ export default function Dashboard() {
 
     }
 
+    const duplicateBookings = bookings.filter((booking, _, allBookings) => {
+        const { date, slot } = booking;
+        // Count how many bookings share the same date
+        const count = allBookings.filter(b => b.date === date && b.slot === slot).length;
+        // Return only bookings where the count is more than 1
+        return count > 1;
+      });
+
+    function handleNavigate(id){
+        navigate(`/admin/bookings/${id}`)
+
+    }
+
     return (
         <>
             <AdminNav />
             <FetchBookings setBookings={setBookings} setLoading={setLoading} />
+            {duplicateBookings.length > 0 ? 
+                <div className="text-center my-8">
+                    <h2 className="text-lg font-semibold text-gray-800">Duplicate Bookings</h2>
+                    <div className="space-y-4">
+                        {duplicateBookings.map((booking) => (
+                            <button 
+                                onClick={() => handleNavigate(booking._id)} 
+                                key={booking._id} 
+                                className="bg-red-500 text-white px-4 py-2 rounded-md mx-1 hover:bg-red-600"
+                            >
+                                {booking.last_name} - {new Date(booking.date).toLocaleDateString()}
+                            </button>
+                        ))}
+                    </div>
+                </div> 
+                : <div></div>}
             {bookings.length > 0 && !loading ?
             <Calendar bookings={bookings} loading={loading} handleClick={handleClick} handleEdit={handleEdit} showNames={true} />
                 : <div className="text-center">Unable to fetch availability</div>
